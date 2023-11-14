a080e68ef77664f83a02c83052823932723c69cd
yassine walid <yassine1998walid@gmail.com>
2023-11-14 15:48:12 +0100

    Create Entities-Repos-Controllers and services Register Admin

--- src/main/java/com/example/jobportalbackend/JobPortalBackendApplication.java
+++ src/main/java/com/example/jobportalbackend/JobPortalBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.jobportalbackend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class JobPortalBackendApplication {
 
     public static void main(String[] args) {
